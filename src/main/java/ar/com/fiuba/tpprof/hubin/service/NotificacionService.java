a9416c0f29f8332b8c7f9d35e05f7c3d288ab9a4
Julián Scialabba <julian.scialabba@gmail.com>
2019-03-21 01:51:39 -0300

    Empiezo con estructura de notfiicaciones

---
+++ src/main/java/ar/com/fiuba/tpprof/hubin/service/NotificacionService.java
@@ -0,0 +1,132 @@
+package ar.com.fiuba.tpprof.hubin.service;
+
+import ar.com.fiuba.tpprof.hubin.dto.AlumnoNotificacionRequestDTO;
+import ar.com.fiuba.tpprof.hubin.exception.InvalidNotificacionException;
+import ar.com.fiuba.tpprof.hubin.model.*;
+import ar.com.fiuba.tpprof.hubin.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificacionService {
+
+	@Autowired
+	private NotificacionDao notificacionDao;
+	@Autowired
+	private DocumentoDao documentoDao;
+	@Autowired
+	private MateriaDao materiaDao;
+	@Autowired
+	private AlumnoDao alumnoDao;
+	@Autowired
+	private AlumnoDocumentoObservableDao alumnoDocumentoObservableDao;
+
+	@Autowired
+	private AlumnoMateriaObservableDao alumnoMateriaObservableDao;
+
+	public void notificarNuevoComentario(Documento documento){
+		for (AlumnoDocumentoObservable alumnoDocObservable: alumnoDocumentoObservableDao.getAllByDocumento(documento)) {
+			Alumno alumno = alumnoDocObservable.getAlumno();
+			Notificacion notificacion = new Notificacion();
+			notificacion.setAlumno(alumno);
+			notificacion.setDocumentoRelacionado(documento);
+			notificacion.setEvento(Notificacion.EVENT_COMMENT);
+			notificacionDao.save(notificacion);
+		}
+	}
+	public void notificarActualizacion(Documento documento){
+		for (AlumnoDocumentoObservable alumnoDocObservable: alumnoDocumentoObservableDao.getAllByDocumento(documento)) {
+			Alumno alumno = alumnoDocObservable.getAlumno();
+			Notificacion notificacion = new Notificacion();
+			notificacion.setAlumno(alumno);
+			notificacion.setDocumentoRelacionado(documento);
+			notificacion.setEvento(Notificacion.EVENT_UPDATE);
+			notificacionDao.save(notificacion);
+		}
+	}
+
+	public void notificarNuevoComentario(Materia materia){
+		for (AlumnoMateriaObservable alumnoMatObservable: alumnoMateriaObservableDao.getAllByMateria(materia)) {
+			Alumno alumno = alumnoMatObservable.getAlumno();
+			Notificacion notificacion = new Notificacion();
+			notificacion.setAlumno(alumno);
+			notificacion.setMateriaRelacionada(materia);
+			notificacion.setEvento(Notificacion.EVENT_COMMENT);
+			notificacionDao.save(notificacion);
+		}
+	}
+
+	public void notificarNuevoDocumento(Materia materia){
+		for (AlumnoMateriaObservable alumnoMatObservable: alumnoMateriaObservableDao.getAllByMateria(materia)) {
+			Alumno alumno = alumnoMatObservable.getAlumno();
+			Notificacion notificacion = new Notificacion();
+			notificacion.setAlumno(alumno);
+			notificacion.setMateriaRelacionada(materia);
+			notificacion.setEvento(Notificacion.EVENT_NEW_DOCUMENT);
+			notificacionDao.save(notificacion);
+		}
+	}
+
+	public void suscribirse(AlumnoNotificacionRequestDTO alumnoNotificacionRequestDTO) throws InvalidNotificacionException{
+		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Alumno alumno = alumnoDao.findByUsername(userDetails.getUsername());
+		if (alumno == null)
+			throw new InvalidNotificacionException("Alumno desconocido");
+
+		String documentoId = alumnoNotificacionRequestDTO.getDocumentoId();
+		String materiaId = alumnoNotificacionRequestDTO.getMateriaId();
+		if((documentoId == null || documentoId.isEmpty()) && (materiaId == null || materiaId.isEmpty())){
+			throw new InvalidNotificacionException("Parametros invalidos");
+		}
+		if(documentoId != null && !documentoId.isEmpty()){
+			Documento documento = documentoDao.findOne(Integer.getInteger(documentoId));
+			AlumnoDocumentoObservable alumnoDocumentoObservable = alumnoDocumentoObservableDao.getAlumnoDocumentoObservableByAlumnoAndDocumento(alumno, documento);
+			if(alumnoDocumentoObservable == null){
+				alumnoDocumentoObservable = new AlumnoDocumentoObservable();
+				alumnoDocumentoObservable.setAlumno(alumno);
+				alumnoDocumentoObservable.setDocumento(documento);
+				alumnoDocumentoObservableDao.save(alumnoDocumentoObservable);
+			}
+		}
+		if(materiaId != null && !materiaId.isEmpty()){
+			Materia materia = materiaDao.findOne(Integer.getInteger(materiaId));
+			AlumnoMateriaObservable alumnoMateriaObservable = alumnoMateriaObservableDao.getAlumnoMateriaObservableByAlumnoAndMateria(alumno, materia);
+			if(alumnoMateriaObservable == null){
+				alumnoMateriaObservable= new AlumnoMateriaObservable();
+				alumnoMateriaObservable.setAlumno(alumno);
+				alumnoMateriaObservable.setMateria(materia);
+				alumnoMateriaObservableDao.save(alumnoMateriaObservable);
+			}
+		}
+	}
+
+	public void desuscribirse(AlumnoNotificacionRequestDTO alumnoNotificacionRequestDTO) throws InvalidNotificacionException{
+		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Alumno alumno = alumnoDao.findByUsername(userDetails.getUsername());
+		if (alumno == null)
+			throw new InvalidNotificacionException("Alumno desconocido");
+
+		String documentoId = alumnoNotificacionRequestDTO.getDocumentoId();
+		String materiaId = alumnoNotificacionRequestDTO.getMateriaId();
+		if((documentoId == null || documentoId.isEmpty()) && (materiaId == null || materiaId.isEmpty())){
+			throw new InvalidNotificacionException("Parametros invalidos");
+		}
+		if(documentoId != null && !documentoId.isEmpty()){
+			Documento documento = documentoDao.findOne(Integer.getInteger(documentoId));
+			AlumnoDocumentoObservable alumnoDocumentoObservable = alumnoDocumentoObservableDao.getAlumnoDocumentoObservableByAlumnoAndDocumento(alumno, documento);
+			if(alumnoDocumentoObservable != null){
+				alumnoDocumentoObservableDao.delete(alumnoDocumentoObservable);
+			}
+		}
+		if(materiaId != null && !materiaId.isEmpty()){
+			Materia materia = materiaDao.findOne(Integer.getInteger(materiaId));
+			AlumnoMateriaObservable alumnoMateriaObservable = alumnoMateriaObservableDao.getAlumnoMateriaObservableByAlumnoAndMateria(alumno, materia);
+			if(alumnoMateriaObservable != null){
+				alumnoMateriaObservableDao.delete(alumnoMateriaObservable);
+			}
+		}
+	}
+
+}
